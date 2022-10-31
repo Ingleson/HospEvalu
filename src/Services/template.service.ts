a97b3cc8edd80febc8ff79e789f8e0d3f97bab0d
Raphael <rapha.gloria@gmail.com>
2022-10-31 16:25:08 -0300

    feat: criei um novo erro para rota /template

--- src/Services/template.service.ts
+++ src/Services/template.service.ts
@@ -2,7 +2,7 @@ import { AppError } from "../Error/appError"
 
 const templateService = async (email: string) => {
   if (!email) {
-    throw new AppError(400, "email errado")
+    throw new AppError(400, "email is required")
   }
 
   return email
