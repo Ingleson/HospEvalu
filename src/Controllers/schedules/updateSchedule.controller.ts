1d188dbf7b7c4568830ac39c629c919f900909ab
Raphael Gloria <rapha.gloria@gmail.com>
2022-11-08 18:06:26 -0300

    fix: rota de atualizar schedule corrigida

--- src/Controllers/schedules/updateSchedule.controller.ts
+++ src/Controllers/schedules/updateSchedule.controller.ts
@@ -1,22 +1,24 @@
-import { Request, Response } from "express";
-import { AppError, handleError } from "../../Error/appError";
-import updateSchedulesService from "../../Services/schedules/updateSchedule.service";
+import { Request, Response } from "express"
+import { AppError, handleError } from "../../Error/appError"
+import updateSchedulesService from "../../Services/schedules/updateSchedule.service"
 
 const updateScheduleController = async (req: Request, res: Response) => {
-    const { id } = req.params
-    const { day, hour, description } = req.body
+  const { id } = req.params
+  const { day, hour, description } = req.body
 
-    try {
-        const update = await updateSchedulesService({id, day, hour, description})
+  try {
+    const update = await updateSchedulesService({ id, day, hour, description })
 
-        if(update){
-            return res.status(204).send("Agendamento atualizado com sucesso!")
-        }
-    } catch (error) {
-        if(error instanceof AppError) {
-            handleError(error, res)
-        }
+    if (update) {
+      return res
+        .status(200)
+        .json({ message: "Agendamento atualizado com sucesso!" })
     }
+  } catch (error) {
+    if (error instanceof AppError) {
+      handleError(error, res)
+    }
+  }
 }
 
 export default updateScheduleController
--- src/Routes/schedules.routes.ts
+++ src/Routes/schedules.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import createScheduleController from "../Controllers/schedules/createSchedule.controller";
 import deleteScheduleController from "../Controllers/schedules/deleteSchedule.controller";
 import getAllSchedulesController from "../Controllers/schedules/getAllSchedules.controller";
+import getSchedulesByProfessionalController from "../Controllers/schedules/getSchedulesByProfessional.controller";
 import getSchedulesByUserController from "../Controllers/schedules/getSchedulesByUser.controller";
 import updateScheduleController from "../Controllers/schedules/updateSchedule.controller";
 import ensureAuthMiddleware from "../Middlewares/ensureAuth.middleware";
@@ -11,6 +12,6 @@ export const scheduleRoutes = Router()
 scheduleRoutes.post("", ensureAuthMiddleware, createScheduleController)
 scheduleRoutes.get("", ensureAuthMiddleware, getAllSchedulesController)
 scheduleRoutes.get("/user/:userId", ensureAuthMiddleware, getSchedulesByUserController)
-scheduleRoutes.get("/professional/:professionalId", ensureAuthMiddleware, getSchedulesByUserController)
+scheduleRoutes.get("/professional/:professionalId", ensureAuthMiddleware, getSchedulesByProfessionalController)
 scheduleRoutes.patch("/:id", ensureAuthMiddleware, updateScheduleController)
 scheduleRoutes.delete("/:id", ensureAuthMiddleware, deleteScheduleController)
